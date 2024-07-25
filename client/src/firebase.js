fdd7a72e2af2bac5666f987950d850affce34bb8
yungkim <gim.yoong@gmail.com>
2024-07-25 15:21:17 -0700

    Secure Firebase configuration using environment variables

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,14 +5,14 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAY9ot4V_xi_Nxd-BmnTrgPCdyjA64__E0",
-    authDomain: "practicelogginglf.firebaseapp.com",
-    projectId: "practicelogginglf",
-    storageBucket: "practicelogginglf.appspot.com",
-    messagingSenderId: "1028509773955",
-    appId: "1:1028509773955:web:6560ca87683e2e8e466bb6",
-    measurementId: "G-EWVHYBHK18"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
